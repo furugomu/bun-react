a998d690cffbd7823c57fb67811fc23adbddaa12
furugomu <furugomu@gmail.com>
2025-02-13 22:40:43 +0900

    :poop:

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bun React + Tailwind CSS Template
+
+```bash
+bun create furugomu/bun-react my-app
+```
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-6xl">こんにちは</h1>
+    </div>
+  );
+};
